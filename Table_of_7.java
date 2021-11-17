931e0eca106f8dc99067f754897786c97d70090f
Ajaykamble2505 <94159371+Ajaykamble2505@users.noreply.github.com>
2021-11-17 20:28:10 +0530

    Add files via upload

---
+++ Table_of_7.java
@@ -0,0 +1,11 @@
+public class Table_of_7  
+{  
+public static void main(String[] args)   
+    {  
+        int a=7;
+        for(int num=1; num>=20; num=num+1){
+            System.out.println(num*a);
+        }
+    }
+}
+    
