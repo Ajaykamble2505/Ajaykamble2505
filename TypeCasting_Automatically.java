58587efa59a8c1f1c73596faf3b657650308594a
Ajaykamble2505 <94159371+Ajaykamble2505@users.noreply.github.com>
2021-11-17 20:28:31 +0530

    Add files via upload

---
+++ TypeCasting_Automatically.java
@@ -0,0 +1,11 @@
+public class TypeCasting_Automatically {
+  public static void main(String[] args) {
+
+    int a = 77;
+    System.out.println("The integer value: " + a);
+
+
+    double b = a;
+    System.out.println("The double value: " + b);
+  }
+}
