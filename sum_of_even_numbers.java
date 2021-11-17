e36ac5ea347ae20b644152ea205e47c2b02255da
Ajaykamble2505 <94159371+Ajaykamble2505@users.noreply.github.com>
2021-11-17 20:27:22 +0530

    Add files via upload

---
+++ sum_of_even_numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class sum_of_even_numbers
+{
+	public static void main(String[] args) {
+	    Scanner in= new Scanner(System.in);
+	    
+		System.out.print("Enter the value of n: ");
+		int n = in.nextInt();
+		
+		int sum=0, num = 1;
+		
+		for(int count=1; count<=n; ){
+
+		    if(num%2 == 0){
+		        sum += num;
+		        count++;
+		    }
+		    num++;
+		}
+		
+		System.out.println("Sum: "+sum);
+	}
+}
