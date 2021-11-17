8eb5873c9eaffa2d294b1b7930fe2a636d6ef530
Ajaykamble2505 <94159371+Ajaykamble2505@users.noreply.github.com>
2021-11-17 20:28:52 +0530

    Add files via upload

---
+++ Typecasting_Manually.java
@@ -0,0 +1,10 @@
+public class Typecasting_Manually {
+  public static void main(String[] args) {
+
+    double a = 99.99;
+    System.out.println("The double value is =  " + a);
+
+    int b = (int)a;
+    System.out.println("The integer value is=  " + b);
+  }
+}
