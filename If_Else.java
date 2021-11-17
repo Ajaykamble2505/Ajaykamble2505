d1543976cd72dd1e007ae798b69333c8adfc37ea
Ajaykamble2505 <94159371+Ajaykamble2505@users.noreply.github.com>
2021-11-17 20:23:52 +0530

    Add files via upload

---
+++ If_Else.java
@@ -0,0 +1,18 @@
+public class If_Else {
+    public static void main(String args[]) {
+       
+        int a=1234;
+        int b=1345;
+        
+        System.out.println("a=" +a);
+        System.out.println("b=" +b);
+        
+        if(a>b) {
+            System.out.println("ans=" +a);
+        }
+        else {
+            System.out.println("ans=" +b);
+        }
+        
+    }
+}
