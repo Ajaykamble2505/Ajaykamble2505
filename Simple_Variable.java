cc130f7c3edf8c7b8b9ac94e446607ac9fd94a9a
Ajaykamble2505 <94159371+Ajaykamble2505@users.noreply.github.com>
2021-11-17 20:27:04 +0530

    Add files via upload

---
+++ Simple_Variable.java
@@ -0,0 +1,10 @@
+public class Simple_Variable{
+    public static void main(String[]args) { 
+        int a=55;
+        float f=a+0;
+        System.out.println(a);
+        System.out.println(f);
+        
+    }
+    
+}
